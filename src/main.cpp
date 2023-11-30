37edae582c5eab331d2538af4859c728becaa0f9
xJunko <fireredz69420@gmail.com>
2023-12-01 00:19:28 +0800

    Main: Update

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "sokol_log.h"
 
 // Internal
+#include "framework/audio/audio.cpp"
 #include "framework/graphics/sprite/sprite.cpp"
 
 namespace TypeT {
@@ -20,8 +21,10 @@ class Context {
 public:
   std::unique_ptr<AssetManager> assets;
   std::unique_ptr<Sprite> sprite;
+  std::unique_ptr<Audio::BASS> audio;
 
   int time;
+  std::shared_ptr<Transform::Transform> transform;
 
   // FNs
   void begin();
@@ -47,6 +50,7 @@ void Context::end() {
 void initialize(void *user_data) {
   Context *data = static_cast<Context *>(user_data);
 
+  // Graphic pipeline
   sg_desc desc = {
       .logger.func = slog_func,
       .context = sapp_sgcontext(),
@@ -56,16 +60,38 @@ void initialize(void *user_data) {
   sgl_desc_t sgl_desc = {};
   sgl_setup(&sgl_desc);
 
+  // Audio pipeline
+  data->audio = std::make_unique<Audio::BASS>();
+  data->audio->initialize();
+
   // Init(s) fields
   data->assets = std::make_unique<AssetManager>();
 
   data->sprite = SpriteUtils::create_sprite_from_image_path(
       "/home/junko/Downloads/junko.png");
   std::printf("Valid: %b \n", data->sprite->textures[0]->valid);
+
+  // Transform TEST
+  data->transform = std::make_shared<Transform::Transform>();
+
+  data->transform->type = Transform::Type::MOVE;
+
+  data->transform->before.push_back(0.0f);
+  data->transform->before.push_back(0.0f);
+
+  data->transform->after.push_back(320.0f);
+  data->transform->after.push_back(240.0f);
+
+  data->transform->time.start = 100.0f;
+  data->transform->time.end = 200.0f;
+
+  data->sprite->transforms.push_back(data->transform);
 }
 
 void draw(void *user_data) {
   Context *ctx = static_cast<Context *>(user_data);
+
+  std::printf("%i \n", ctx->time);
 
   {
     ctx->time++;
@@ -78,6 +104,7 @@ void draw(void *user_data) {
   ctx->begin();
 
   if (ctx->time < 500) {
+    ctx->sprite->update(float(ctx->time));
     ctx->sprite->draw();
   }
 
