ef90e950c14cde933022914fa4874ef027fe3ec5
xJunko <fireredz69420@gmail.com>
2023-12-01 00:52:23 +0800

    Audio: Implement `Track` and `.play/pause/resume`

--- src/framework/audio/audio.cpp
+++ src/framework/audio/audio.cpp
@@ -1,6 +1,7 @@
 #pragma once
 
 // OK
+#include <memory>
 #include <stdbool.h>
 #define BOOL bool
 
@@ -52,6 +53,62 @@ void BASS::initialize() {
   } else {
     std::printf("[Audio] Error, failed to start BASS!");
   }
+}
+
+class Track {
+private:
+  HSTREAM channel;
+
+public:
+  std::shared_ptr<BASS> bass;
+  std::string path;
+
+  void initialize();
+  void play();
+  void pause();
+  void resume();
+};
+
+void Track::initialize() {
+  channel = BASS_StreamCreateFile(0, path.c_str(), 0, 0,
+                                  BASS_STREAM_DECODE | BASS_STREAM_PRESCAN |
+                                      BASS_ASYNCFILE);
+
+  // FX
+  channel =
+      BASS_FX_TempoCreate(channel, BASS_FX_FREESOURCE | BASS_STREAM_DECODE);
+
+  BASS_ChannelSetAttribute(channel, BASS_ATTRIB_TEMPO_OPTION_USE_QUICKALGO, 1);
+  BASS_ChannelSetAttribute(channel, BASS_ATTRIB_TEMPO_OPTION_OVERLAP_MS, 4.0);
+  BASS_ChannelSetAttribute(channel, BASS_ATTRIB_TEMPO_OPTION_SEQUENCE_MS, 30.0);
+
+  // HACK: volume
+  BASS_ChannelSetAttribute(channel, BASS_ATTRIB_VOL, 0.2f);
+}
+
+void Track::play() {
+  BASS_Mixer_StreamAddChannel(
+      bass->mixer, channel, BASS_MIXER_CHAN_NORAMPIN | BASS_MIXER_CHAN_BUFFER);
+}
+
+void Track::pause() {
+  BASS_Mixer_ChannelFlags(channel, BASS_MIXER_CHAN_PAUSE,
+                          BASS_MIXER_CHAN_PAUSE);
+}
+
+void Track::resume() {
+  BASS_Mixer_ChannelFlags(channel, 0, BASS_MIXER_CHAN_PAUSE);
+}
+
+std::shared_ptr<Track> new_track(std::string path, std::shared_ptr<BASS> bass) {
+  auto track = std::make_shared<Track>();
+
+  track->path = path;
+  track->bass = bass;
+
+  track->initialize();
+
+  return track;
 }
 
 } // namespace Audio
