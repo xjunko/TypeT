d301bfe3cacd8e1ba93af95e9b74e461b9980010
xJunko <fireredz69420@gmail.com>
2023-12-07 23:24:03 +0800

    Storyboard: Add `StoryboardUtils`

---
+++ src/framework/storyboard/storyboard_utils.cpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../language/string.cpp"
+#include <string>
+#include <vector>
+
+namespace StoryboardUtils {
+
+static inline std::string parse_header(std::string s) {
+  if (s.rfind("[", 0) == 0) {
+    auto ss = s.replace(s.find("["), sizeof("[") - 1, "")
+                  .replace(s.find("]"), sizeof("]") - 1, "");
+
+    StringUtils::trim(ss);
+
+    return ss;
+  }
+
+  return "";
+}
+
+static inline std::vector<std::string> parse_variables(std::string s) {
+  auto ss = s;
+  StringUtils::trim(ss);
+  return StringUtils::split(ss, "=");
+}
+
+} // namespace StoryboardUtils
