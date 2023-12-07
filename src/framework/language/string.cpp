4dda56b1375b794ddad457177d7ebbbc942386e7
xJunko <fireredz69420@gmail.com>
2023-12-07 23:23:53 +0800

    Framework: Add `StringUtils`

---
+++ src/framework/language/string.cpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <iterator>
+#include <locale>
+
+namespace StringUtils {
+// Straight from SO
+// https://stackoverflow.com/questions/216823/how-to-trim-a-stdstring
+
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+            return !std::isspace(ch);
+          }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(),
+                       [](unsigned char ch) { return !std::isspace(ch); })
+              .base(),
+          s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+  rtrim(s);
+  ltrim(s);
+}
+
+// trim from start (copying)
+static inline std::string ltrim_copy(std::string s) {
+  ltrim(s);
+  return s;
+}
+
+// trim from end (copying)
+static inline std::string rtrim_copy(std::string s) {
+  rtrim(s);
+  return s;
+}
+
+// trim from both ends (copying)
+static inline std::string trim_copy(std::string s) {
+  trim(s);
+  return s;
+}
+
+static inline std::vector<std::string> split(std::string s, std::string delim) {
+  std::stringstream ss(s);
+  std::istream_iterator<std::string> begin(ss);
+  std::istream_iterator<std::string> end;
+  std::vector<std::string> vstrings(begin, end);
+
+  return vstrings;
+}
+
+} // namespace StringUtils
