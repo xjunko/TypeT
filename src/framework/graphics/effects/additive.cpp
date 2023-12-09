be25931a44c25e462bce64672d0378d38b025bd4
xJunko <fireredz69420@gmail.com>
2023-12-10 02:03:04 +0800

    Effects: Compiler being a bitch

--- src/framework/graphics/effects/additive.cpp
+++ src/framework/graphics/effects/additive.cpp
@@ -12,12 +12,14 @@ sgl_pipeline PIPELINE;
 sgl_pipeline get() {
   if (!INIT) {
     sg_pipeline_desc desc = {
+        .colors[0] = {.blend =
+                          sg_blend_state{
+                              .enabled = true,
+                              .src_factor_rgb = SG_BLENDFACTOR_SRC_ALPHA,
+                              .dst_factor_rgb = SG_BLENDFACTOR_ONE,
+                          }},
         .label = "additive",
-        .colors[0] = {.blend = sg_blend_state{
-                          .enabled = true,
-                          .src_factor_rgb = SG_BLENDFACTOR_SRC_ALPHA,
-                          .dst_factor_rgb = SG_BLENDFACTOR_ONE,
-                      }}};
+    };
     PIPELINE = sgl_make_pipeline(desc);
     INIT = true;
   }
--- src/framework/graphics/effects/normal.cpp
+++ src/framework/graphics/effects/normal.cpp
@@ -12,12 +12,15 @@ sgl_pipeline PIPELINE;
 sgl_pipeline get() {
   if (!INIT) {
     sg_pipeline_desc desc = {
+        .colors[0] = {.blend =
+                          sg_blend_state{
+                              .enabled = true,
+                              .src_factor_rgb = SG_BLENDFACTOR_SRC_ALPHA,
+                              .dst_factor_rgb =
+                                  SG_BLENDFACTOR_ONE_MINUS_SRC_ALPHA,
+                          }},
         .label = "alpha",
-        .colors[0] = {.blend = sg_blend_state{
-                          .enabled = true,
-                          .src_factor_rgb = SG_BLENDFACTOR_SRC_ALPHA,
-                          .dst_factor_rgb = SG_BLENDFACTOR_ONE_MINUS_SRC_ALPHA,
-                      }}};
+    };
     PIPELINE = sgl_make_pipeline(desc);
     INIT = true;
   }
